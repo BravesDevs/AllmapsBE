bdd46f349019e0db622bf0b0573aa74cfbfd54c8
Devang Parekh <devangparekh2014@gmail.com>
2024-05-14 12:43:28 -0400

    Revise the debug consoles

--- src/services/proc.ts
+++ src/services/proc.ts
@@ -12,10 +12,13 @@ const model = genAI.getGenerativeModel({ model: 'gemini-1.5-pro-latest' });
 
 export const generateResult = async (input: String, level: String) => {
     try {
+        console.log("Input:", input);
+        console.log("Level:", level);
         const prompt = process.env.MODEL_PROMPT + input + ', Level: ' + level;
         const result = await model.generateContent(prompt);
-
+        console.log("Generating Response...");
         const response = await result.response;
+        console.log("Response:", response);
         let text = response.text();
 
         return extractJson(text);
